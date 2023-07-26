2ce32f95f5acfb1a12d24be43d04dfb1b405daf0
Albert Kidd <475555354@qq.com>
2023-07-27 00:00:57 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          牛果AI小助手🥑
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Ask me anything.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
